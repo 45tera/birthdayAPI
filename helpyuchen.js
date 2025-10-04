80f7e740e923ca7f9d27ae134fd6e29328f7787a
45tera <45terax@gmail.com>
2025-10-05 00:24:59 +0800

    feat: can grab nendroid

--- helpyuchen.js
+++ helpyuchen.js
@@ -1,32 +1,57 @@
 const express = require('express')
 const app = express()
 const port = 5678;
+const nendroidsList = require("./src/nendroids.json"); // import the list
 
-const users = [
-  {
-    name: 'yuchen',
-    user: '1',
-  },
-  {
-    name: 'yuchen2',
-    user: '2',
-  }
-];
+
+
+function calculateNen(birthday) {
+  var {month, day} = birthday;
+  var monthNumber = convertMonthToNumber(month);
+
+  var nenValue = (monthNumber + day);
+  return nenValue
+}
+
+
+function convertMonthToNumber(value) {
+  const monthMap = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+  };
+  return monthMap[value];
+}
 
 
 app.use(express.json());
 
 app.listen(port, () =>{
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`[DEBUG] Server is running on http://localhost:${port}`);
 });
 
 app.get('/', (req, res) => {
-  res.send('2 yuchen helped')
+  res.send('head over to /nendroid?month=YourBirthMonth&day=YourBirthDay to get your nendroid!')
 })
 
 
-app.get('/user', (req,res)=> {
-  res.json(users);
-})
 
-//nodemon
+app.get('/nendroid', (req,res)=> {
+
+  var birthday = req.query;
+
+  var nenValue = calculateNen(birthday);
+
+  var nendroid = nendroidsList.find(item => item.id === Number(nenValue));
+
+  res.send({ nendroid });
+})
